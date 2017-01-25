c71355e0197567a4eabc987122033b35bee542e3
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-01-25 16:20:54 -0600

    1/25/2017

---
+++ App4/InvoiceViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App4
+{
+    public partial class InvoiceViewController : UITableViewController
+    {
+        public InvoiceViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
