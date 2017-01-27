d935e242309f4405790cea9080722487c8ad6882
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-01-26 21:42:05 -0600

    ClientDetailsViewController

---
+++ App4/:Users:jasonding:Projects:Invoice:App4:ClientDetailViewController.cs
@@ -0,0 +1,20 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App4
+{
+    public partial class ClientDetailViewController : UITableViewCont
+    {
+        public ClientDetailViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+
+		}
+    }
+}
---
+++ App4/ClientDetailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App4
+{
+    public partial class ClientDetailsViewController : UITableViewController
+    {
+        public ClientDetailsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
