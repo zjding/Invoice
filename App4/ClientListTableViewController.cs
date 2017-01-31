0061a9a32321517d678014d5c490622cb592b047
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-01-30 21:37:06 -0600

    ClientTableViewControllers

---
+++ App4/ClientListTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App4
+{
+    public partial class ClientListTableViewController : UITableViewController
+    {
+        public ClientListTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
