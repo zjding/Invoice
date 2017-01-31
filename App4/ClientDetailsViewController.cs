2cb3269bfcf37ff826bb15a45a060d51701d5caf
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-01-30 22:05:52 -0600

    Addressbook

--- App4/ClientDetailsViewController.cs
+++ App4/ClientDetailsViewController.cs
@@ -1,13 +1,67 @@
 using Foundation;
 using System;
 using UIKit;
+using AddressBook;
+using AddressBookUI;
 
 namespace App4
 {
-    public partial class ClientDetailsViewController : UITableViewController
-    {
-        public ClientDetailsViewController (IntPtr handle) : base (handle)
-        {
-        }
-    }
+	public partial class ClientDetailsViewController : UITableViewController
+	{
+		public ClientDetailsViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			btnImportFromContacts.Clicked += BtnImportFromContacts_Clicked;
+		}
+
+		void BtnImportFromContacts_Clicked(object sender, EventArgs e)
+		{
+			ABAuthorizationStatus status = ABAddressBook.GetAuthorizationStatus();
+			NSError error;
+			ABAddressBook addressBook =
+			ABAddressBook.Create(out error);
+			if (status == ABAuthorizationStatus.NotDetermined)
+			{
+				addressBook.RequestAccess((g, err) =>
+				{
+					if (g)
+					{
+						this.InvokeOnMainThread(() =>
+						this.DisplayContactCard(addressBook));
+					}
+					else
+					{
+						Console.WriteLine("User denied access to the address book!");
+					}
+				});
+			}
+			else if (status == ABAuthorizationStatus.Authorized)
+			{
+				this.DisplayContactCard(addressBook);
+			}
+			else
+			{
+				Console.WriteLine("App does not have access to the address book!");
+			}
+		}
+
+
+		private void DisplayContactCard(ABAddressBook addressBook)
+		{
+			//ABPerson[] contacts = addressBook.GetPeople();
+			//ABPersonViewController personController =
+			//new ABPersonViewController();
+			//personController.DisplayedPerson = contacts[0];
+			//this.NavigationController.PushViewController(
+			//personController, true);
+
+			ABPeoplePickerNavigationController contactController = new ABPeoplePickerNavigationController();
+			PresentViewController(contactController, true, null);
+		}
+	}
 }
--- App4/ClientDetailsViewController.designer.cs
+++ App4/ClientDetailsViewController.designer.cs
@@ -14,8 +14,16 @@ namespace App4
     [Register ("ClientDetailsViewController")]
     partial class ClientDetailsViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem btnImportFromContacts { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (btnImportFromContacts != null) {
+                btnImportFromContacts.Dispose ();
+                btnImportFromContacts = null;
+            }
         }
     }
 }
