52a8f5718527b2a57e87e76abc979b307938e344
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-02-05 08:47:52 -0600

    Raised Tab button

---
+++ App4/InvoiceTabBarController.cs
@@ -0,0 +1,50 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace App4
+{
+	public partial class InvoiceTabBarController : RaisedTabBarController
+    {
+
+		public InvoiceTabBarController(IntPtr handle) : base (handle)
+        {
+
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+
+			this.InsertEmptyTabItem("", 2);
+
+			var img = UIImage.FromFile("Images/Add.png");
+			this.AddRaisedButton(img, null);
+		}
+
+		public override void onRaisedButton_TouchUpInside(object sender, EventArgs e)
+		{
+			// Create a new Alert Controller
+				UIAlertController actionSheetAlert = UIAlertController.Create("Create a new item", "Select an type from below", UIAlertControllerStyle.ActionSheet);
+
+			// Add Actions
+			actionSheetAlert.AddAction(UIAlertAction.Create("New Invoice", UIAlertActionStyle.Default, (action) => Console.WriteLine("Item One pressed.")));
+
+			actionSheetAlert.AddAction(UIAlertAction.Create("New Estimate", UIAlertActionStyle.Default, (action) => Console.WriteLine("Item Two pressed.")));
+
+			actionSheetAlert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, (action) => Console.WriteLine("Cancel button pressed.")));
+
+			// Required for iPad - You must specify a source for the Action Sheet since it is
+			// displayed as a popover
+			//UIPopoverPresentationController presentationPopover = actionSheetAlert.PopoverPresentationController;
+			//if (presentationPopover != null)
+			//{
+			//	presentationPopover.SourceView = this.View;
+			//	presentationPopover.PermittedArrowDirections = UIPopoverArrowDirection.Up;
+			//}
+
+			// Display the alert
+			this.PresentViewController(actionSheetAlert, true, null);
+		}
+    }
+}
---
+++ App4/RaisedTabBarController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace App4
+{
+	public class RaisedTabBarController: UITabBarController 
+	{
+		public RaisedTabBarController()
+		{
+		}
+
+		public RaisedTabBarController(IntPtr handle) : base (handle)
+        {
+
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+
+		public void InsertEmptyTabItem(string title, int i)
+		{
+			var vc = new UIViewController();
+
+			vc.TabBarItem = new UITabBarItem(title, null, 0);
+			vc.TabBarItem.Enabled = false;
+
+			List<UIViewController> viewCtlList = new List<UIViewController>(this.ViewControllers);
+
+			viewCtlList.Insert(i, vc);
+
+			this.ViewControllers = viewCtlList.ToArray();
+		}
+
+		public void AddRaisedButton(UIImage buttonImage, UIImage highlightImage)
+		{
+			var button = new UIButton(UIButtonType.Custom);
+
+			button.AutoresizingMask = UIViewAutoresizing.FlexibleRightMargin |
+				UIViewAutoresizing.FlexibleLeftMargin |
+				UIViewAutoresizing.FlexibleBottomMargin |
+				UIViewAutoresizing.FlexibleTopMargin;
+
+			button.Frame = new CoreGraphics.CGRect(0, 0, buttonImage.Size.Width, buttonImage.Size.Height);
+
+			button.SetBackgroundImage(buttonImage, UIControlState.Normal);
+			button.SetBackgroundImage(highlightImage, UIControlState.Highlighted);
+
+			var heightDifference = buttonImage.Size.Height - this.TabBar.Frame.Size.Height;
+
+			if (heightDifference < 0)
+			{
+				button.Center = this.TabBar.Center;
+			}
+			else
+			{
+				var center = this.TabBar.Center;
+				center.Y -= (nfloat)(heightDifference / 2.0);
+				button.Center = center;
+			}
+
+			button.TouchUpInside += onRaisedButton_TouchUpInside;
+
+			this.View.AddSubview(button);
+		}
+
+		public virtual void onRaisedButton_TouchUpInside(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
