9e383af1498ae2880dc08ea44dbc942321576961
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-02-18 23:20:27 -0600

    imageattachcontroller

--- App4/InvoicePhotoViewController.cs
+++ App4/InvoicePhotoViewController.cs
@@ -16,8 +16,8 @@ namespace App4
 		{
 			base.ViewDidLoad();
 
-			btnCamera.Frame = new RectangleF(0, 0, (float)UIScreen.MainScreen.Bounds.Width / 2, (float)viewButtons.Bounds.Height);
-			btnPhotos.Frame = new RectangleF((float)UIScreen.MainScreen.Bounds.Width / 2, 0, (float)UIScreen.MainScreen.Bounds.Width / 2, (float)viewButtons.Bounds.Height);
+			//btnCamera.Frame = new RectangleF(0, 0, (float)UIScreen.MainScreen.Bounds.Width / 2, (float)viewButtons.Bounds.Height);
+			//btnPhotos.Frame = new RectangleF((float)UIScreen.MainScreen.Bounds.Width / 2, 0, (float)UIScreen.MainScreen.Bounds.Width / 2, (float)viewButtons.Bounds.Height);
 
 
 		}
--- App4/InvoicePhotoViewController.designer.cs
+++ App4/InvoicePhotoViewController.designer.cs
@@ -14,34 +14,8 @@ namespace App4
     [Register ("InvoicePhotoViewController")]
     partial class InvoicePhotoViewController
     {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton btnCamera { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton btnPhotos { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIView viewButtons { get; set; }
-
         void ReleaseDesignerOutlets ()
         {
-            if (btnCamera != null) {
-                btnCamera.Dispose ();
-                btnCamera = null;
-            }
-
-            if (btnPhotos != null) {
-                btnPhotos.Dispose ();
-                btnPhotos = null;
-            }
-
-            if (viewButtons != null) {
-                viewButtons.Dispose ();
-                viewButtons = null;
-            }
         }
     }
 }
