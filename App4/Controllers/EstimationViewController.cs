905e2caa5c6d87fdbdb2fd68fac1622908848455
zjding <jasonding@Jasons-MacBook-Pro.local>
2017-03-01 21:29:02 -0600

    3/1/2017

--- App4/Controllers/EstimationViewController.cs
+++ App4/Controllers/EstimationViewController.cs
@@ -2,6 +2,7 @@ using Foundation;
 using System;
 using UIKit;
 using BLKFlexibleHeightBarDemo;
+using CoreGraphics;
 
 namespace App4
 {
@@ -19,7 +20,7 @@ namespace App4
 
 			SetNeedsStatusBarAppearanceUpdate();
 
-			_headerBar = new SquareCashStyleBar(new CoreGraphics.CGRect(0f, 0f, View.Frame.Width, 100f));
+			_headerBar = new SquareCashStyleBar(new CGRect(0f, 0f, View.Frame.Width, 50f));
 
 			var behaviorDefiner = new SquareCashStyleBehaviorDefiner();
 			behaviorDefiner.AddSnappingPositionProgress(0f, 0f, .5f);
@@ -30,11 +31,25 @@ namespace App4
 			_headerBar.BehaviorDefiner = behaviorDefiner;
 
 			View.AddSubview(_headerBar);
+
+			tableView.ContentInset = new UIEdgeInsets(_headerBar.MaximumBarHeight, 0f, 0f, 0f);
+
+			var closeButton = new BLKFlexibleHeightBarSubviewUIButton();
+			closeButton.Frame = new CGRect(0f, 0f, 30f, 30f);
+			closeButton.TintColor = UIColor.White;
+			closeButton.SetImage(UIImage.FromFile(@"Images/Multiply-50-white.png"), UIControlState.Normal);
+			//closeButton.AddTarget(CloseViewController, UIControlEvent.TouchUpInside);
+			_headerBar.AddSubview(closeButton);
 		}
 
 		public override UIStatusBarStyle PreferredStatusBarStyle()
 		{
 			return UIStatusBarStyle.LightContent;
+		}
+
+		public override bool PrefersStatusBarHidden()
+		{
+			return true;
 		}
     }
 }
